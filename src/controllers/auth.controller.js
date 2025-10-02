97aa995298e57a2b1caeb5eb5bf27dd7bc39b93b
Eric <patinetakiller564@gmail.com>
2025-10-01 22:34:24 -0300

    A

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,27 @@
+import { hashPassword } from "../helpers/bcrypt.helper";
+import { UserModel } from "../models/mongoose/user.model";
+
 export const register = async (req, res) => {
+  const { username, email, password, role, profile } = req.body;
   try {
-    // TODO: crear usuario con password hasheada y profile embebido
-    return res.status(201).json({ msg: "Usuario registrado correctamente" });
+    const existEmail = await UserModel.findOne({ email: email });
+    if(existEmail){
+      return res.status(200).json({msg:"Correo Ya Registrado"})
+    }
+    const hashedPassword = await hashPassword(password);
+    const user = await UserModel.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+      profile,
+    });
+    if (!user) {
+      return res.status(400).json({ ok: false, msg: "No Creado", data: null });
+    }
+    return res
+      .status(201)
+      .json({ msg: "Usuario registrado correctamente", data: user });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Error interno del servidor" });
